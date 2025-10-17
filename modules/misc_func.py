finally the correct phase transition plot... noisy order parameter was due to correlated sampling and, mainly, transient data being used to calculate the average

modules/misc_func.py
@@ -101,7 +101,7 @@ def get_par_value_from_str(str_list,par_name,dtype=int):
     if type(str_list) is str:
         return dtype(match.group(1)) if (match := re.search(par_name + r'(\d+(?:\.\d+)?)', str_list)) else None
     else:
-        return [get_par_value_from_str(idl,par_name) for idl in str_list]
+        return [get_par_value_from_str(idl,par_name,dtype) for idl in str_list]
 
 def par_value_in_str(str_list,par_name,par_value,dtype=int,par_str_fmt=None):
     """
@@ -172,13 +172,35 @@ def get_percent_load_str(k,n_tot):
 def nonempty_str(X):
     return (type(X) is str) and (len(X)>0)
 
+#@njit([
+#    types.optional(types.int64)(types.float64[:],types.float64),
+#    types.optional(types.int64)(types.int64[:],types.int64)
+#])
+@njit
 def find_first(X,v):
-    k = numpy.argmax(X==v)
-    return k if X[k]==v else None
+    for k,x in enumerate(X):
+        if x == v:
+            return k
+    return None
 
+#@njit([
+#    types.optional(types.int64)(types.float64[:],types.float64),
+#    types.optional(types.int64)(types.int64[:],types.int64)
+#])
+@njit
 def find_last(X,v):
-    k = X.size - 1 - numpy.argmax(X[::-1]==v)
-    return k if X[k]==v else None
+    for k in range(len(X)-1,-1,-1):
+        if X[k] == v:
+            return k
+    return None
+
+#def find_first(X,v):
+#    k = numpy.argmax(X==v)
+#    return k if X[k]==v else None
+
+#def find_last(X,v):
+#    k = X.size - 1 - numpy.argmax(X[::-1]==v)
+#    return k if X[k]==v else None
 
 def unpack_list_of_tuples(lst):
     """
@@ -241,7 +263,21 @@ def _split_array(a, n):
     k, m = divmod(len(a), n)
     return numpy.array([ a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n) ])
 
-def linearized_fit(x_data, y_data, x_transform, y_transform, inverse_transform, mask=None):
+def sort_A_to_B(A,B,return_ind=False):
+    """
+    sorts A according to B
+    returns A, the indices that sort A [[ A[ind] == A_sorted  ]]
+    """
+    map_AB    = { b:k for k,b in enumerate(B) }
+    ind,A_srt = unpack_list_of_tuples(sorted(list(enumerate(A)),key=lambda pair:map_AB[pair[1]]))
+    if _is_numpy_array(A):
+        A_srt = numpy.array(A_srt)
+    if return_ind:
+        return A_srt,ind
+    else:
+        return A_srt
+
+def linearized_fit(x_data, y_data, x_transform=None, y_transform=None, y_inverse_transform=None, mask=None):
     """
     Performs linear regression on transformed data using scipy.stats.linregress.
     I.e., this function fits a line
@@ -258,7 +294,7 @@ def linearized_fit(x_data, y_data, x_transform, y_transform, inverse_transform, 
         Function to transform x_data for linearization.
     y_transform : callable
         Function to transform y_data for linearization.
-    inverse_transform : callable
+    y_inverse_transform : callable
         Function to reverse the y_transform for plotting fitted curve in original space.
     mask : numpy-compatible index [e.g., range, int, logical, etc]
         if present, only performs fit on the masked data: x_data[mask], y_data[mask]
@@ -276,6 +312,13 @@ def linearized_fit(x_data, y_data, x_transform, y_transform, inverse_transform, 
     y_fit : ndarray
         Fitted y values in original space.
     """
+    if not exists(x_transform):
+        x_transform = lambda x:x
+    if not exists(y_transform):
+        y_transform         = lambda y:y
+        y_inverse_transform = lambda y:y
+    assert exists(y_transform) and exists(y_inverse_transform), "y_inverse_transform must be set when y_transform is set"
+
     # Transform the data
     if exists(mask):
         x_lin = x_transform(x_data[mask])
@@ -289,11 +332,11 @@ def linearized_fit(x_data, y_data, x_transform, y_transform, inverse_transform, 
 
     # Generate fitted values in original space
     x_fit = numpy.linspace(min(x_data), max(x_data), 100)
-    func  = lambda x,*fitpar: inverse_transform(fitpar[0] * x_transform(x) + fitpar[1])
+    func  = lambda x,*fitpar: y_inverse_transform(fitpar[0] * x_transform(x) + fitpar[1])
     y_fit = func(x_fit,slope,intercept)
 
     # Compute residuals and RSS in original space
-    y_pred    = inverse_transform(slope * x_lin + intercept)
+    y_pred    = y_inverse_transform(slope * x_lin + intercept)
     residuals = (y_data[mask] if exists(mask) else y_data) - y_pred
     rss       = numpy.sum(residuals**2)
 

modules/phasetransdata.py
@@ -158,18 +158,24 @@ def save_phasetrans_file(fname,N_values,pt_data,d_files):
     scipy.io.savemat(fname,{'N':N_values,'data':io.list_of_arr_to_arr_of_obj(pt_data ),'d_files':io.list_of_arr_to_arr_of_obj(d_files)})
 
 
+def _merge_data_phasetrans_struct(s1,s2,f,ind,param_name_control):
+    get_data = lambda s: s[f] if f in s else numpy.full(s[param_name_control].shape,numpy.nan)
+    return numpy.concatenate((get_data(s1).flatten(),get_data(s2).flatten()))[ind]
+
+
 def merge_phasetrans_params_struct(s1,s2,param_name_control):
     if type(s1) is list:
         assert type(s2) is list, 'if s1 is a list, s2 must also be a list (if lengths do not match, only merge up to smallest length between s1 and s2)'
         return [ merge_phasetrans_params_struct(ss1,ss2,param_name_control) for ss1,ss2 in zip(s1,s2) ]
     f1     = s1.keys()
     f2     = s2.keys()
-    fields = set(f1) & set(f2)
-    assert fields == set(f1), 'both input structs must have the same fields'
+    fields = set(f1) | set(f2)
+    #assert fields == set(f1), 'both input structs must have the same fields'
+    assert param_name_control in fields, f'"{param_name_control}" must be a field in both input structs'
     fields.remove(param_name_control)
     p,ind = numpy.unique(numpy.concatenate((s1[param_name_control].flatten(),s2[param_name_control].flatten())), return_index=True)
     d     = {param_name_control:p}
-    d.update({f:numpy.concatenate((s1[f],s2[f]))[ind] for f in fields})
+    d.update({f:_merge_data_phasetrans_struct(s1,s2,f,ind,param_name_control) for f in fields})
     return misc.structtype(**d)
 
 
@@ -230,22 +236,35 @@ def convert_phasetrans_matrix_to_struct(phasetrans_data,param_name_control,param
     res['n_data'                  ] = phasetrans_data[:,6]
     return res
 
+def cutoff_transient(d,orderpar_field,t_trans,time_field='time',dt_field='dt'):
+    if not misc.exists(t_trans):
+        return d
+    if type(d) is list:
+        return [ cutoff_transient(d,orderpar_field,t_trans,time_field=time_field,dt_field=dt_field) for dd in d ]
+    assert all(f in d for f in [orderpar_field,time_field,dt_field]), 'check fields in data variable, some were not found'
+    d[orderpar_field] = d[orderpar_field][d[time_field]*d[dt_field]>=t_trans]
+    d[time_field]     = d[time_field][    d[time_field]*d[dt_field]>=t_trans]
+    return d
+
 def calc_phasetrans_params_struct(
     f_list,
     param_name_control,
     time_k1=None,
     time_k2=None,
+    t_trans=None,
     n_time_steps_sample=None,
     return_file_data=False,
     param_name_orderpar='rho',
     param_name_systemsize='N',
     param_name_time='time',
+    param_name_dt='dt',
     calc_suscept_bootstrap_error=False,
     return_as_struct=True,
     **bootstrap_args
 ):
     """
     Computes phase transition parameters from a list of .mat files containing time series data.
+    Cuts off the first t_trans time steps (in units of d.dt) from the time series before averaging [[ i.e. cuts off from d.time[0]*d.dt to d.time[k]*d.dt==t_trans ]]
     Phase transition quantities must be computed from an uncorrelated order parameter time series
     Use 'n_time_steps_sample' to subsamples the time series by selecting every nth time step.
     This is important to calculate averages ignoring autocorrelated data.
@@ -321,9 +340,9 @@ def calc_phasetrans_params_struct(
     for k,f in progress_bar: # for each input file
         progress_bar.set_postfix_str(f)
         d                    = io.import_mat_file(f,variable_names=variable_names,return_structtype=return_structtype)
-        phasetrans_data[k,:] = calc_phasetrans_params(d,param_name_control,n_time_steps_sample=n_time_steps_sample,calc_suscept_bootstrap_error=calc_suscept_bootstrap_error,**bootstrap_args)
         if return_file_data:
             d_trim.append(select_timerange_from_data(d, time_k1, time_k2))
+        phasetrans_data[k,:] = calc_phasetrans_params(cutoff_transient(d,param_name_orderpar,t_trans,param_name_time,param_name_dt),param_name_control,n_time_steps_sample=n_time_steps_sample,calc_suscept_bootstrap_error=calc_suscept_bootstrap_error,**bootstrap_args)
     progress_bar.close()
     ind             = numpy.argsort(phasetrans_data[:,0])  # sorting according to param value
     phasetrans_data = phasetrans_data[ind,:]